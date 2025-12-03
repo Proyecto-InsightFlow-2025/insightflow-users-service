7e1f64735fa3570df4b4d7eb7b780a4830b81878
godchisa254 <Matias.254@outlook.com>
2025-12-02 21:41:17 -0300

    build(main): adds cors, auth and controller config

--- Program.cs
+++ Program.cs
@@ -1,16 +1,27 @@
+
 using insightflow_users_service.src.Data;
+using insightflow_users_service.src.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()   // Firebase Hosting
+                  .AllowAnyMethod()   // GET, POST, PATCH, DELETE
+                  .AllowAnyHeader();  // X-User-Id header
+        });
+});
 builder.Services.AddSingleton<ApplicationDbContext>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -19,4 +30,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors("AllowAll");
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
